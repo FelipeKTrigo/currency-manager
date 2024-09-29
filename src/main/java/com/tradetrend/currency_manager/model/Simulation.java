af63f19d1f6b2791d23d13834bc26641cf6fb9d2
felipe knoner trigo <feelknotri@gmail.com>
2024-09-29 18:27:26 -0300

    id generation update

--- src/main/java/com/tradetrend/currency_manager/model/Simulation.java
+++ src/main/java/com/tradetrend/currency_manager/model/Simulation.java
@@ -1,10 +1,14 @@
 package com.tradetrend.currency_manager.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,8 +21,8 @@ import java.time.LocalDateTime;
 public class Simulation {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private UUID id;
     private LocalDateTime simulationDateTime;
     private BigDecimal totalAmountExchange;
     private String originCurrency;
